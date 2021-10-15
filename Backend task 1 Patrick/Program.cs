985e8b4a50f98efa86f5047a81cf293d43d67ca9
K. Hermaszewski <hermaszewski.krzysztof@gmail.com>
2021-10-15 20:05:48 +0200

    Add project files.

---
+++ Backend task 1 Patrick/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Backend_task_1_Patrick
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SayHello();
+        }
+        static void SayHello()
+        {
+            Console.Write("Enter your name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Welcome " + userName);
+        }
+    }
+}

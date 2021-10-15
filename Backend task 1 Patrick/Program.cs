a11cf1e0af1686cb8391af2da3e0f0230038f980
K. Hermaszewski <hermaszewski.krzysztof@gmail.com>
2021-10-15 22:40:46 +0200

    Almost done

--- Backend task 1 Patrick/Program.cs
+++ Backend task 1 Patrick/Program.cs
@@ -8,17 +8,65 @@ namespace Backend_task_1_Patrick
         {
             bool closeFlag = true;
 
-            while(closeFlag)
+            while (closeFlag)
             {
+                Console.WriteLine("Choose one from options bellow. Write a number to make a choice");
+                Console.WriteLine("1 - Greet");
+                string greetApp = Console.ReadLine();
+                if (greetApp == "1")
+                {
+                    SayHello();
+                }
+                else if (greetApp == "2")
+                {
+                    CheckerboardPattern();
+                }
+                else
+                {
+                    Console.WriteLine("Bad input. Enter a number between 1 and 4");
+                }
+            }
+
+            static void SayHello()
+            {
+                Console.Write("Enter your name: ");
+                string userName = Console.ReadLine();
+                Console.WriteLine("Welcome " + userName + "!!!");
+            }
+
+            static void CheckerboardPattern()
+            {
+                Console.WriteLine("Make your own pattern");
+                Console.Write("How many colums want you make: ");
+                int column = Convert.ToInt32(Console.ReadLine());
+                Console.Write("How many rows want you make: ");
+                int row = Convert.ToInt32(Console.ReadLine());
+
+                try
+                {
+                    for (int i = 1; i <= column; i++)
+                    {
+                        for (int j = 1; j <= row; j++)
+                        {
+                            if ((i + j) % 2 == 0)
+                            {
+                                Console.Write("X");
+                            }
+                            else
+                            {
+                                Console.Write("O");
+                            }
+                        }
+                        Console.WriteLine();
+                    }
+                } catch
+                {
+                    Console.WriteLine("Bad input.");
+                }
 
             }
-            SayHello();
         }
-        static void SayHello()
-        {
-            Console.Write("Enter your name: ");
-            string userName = Console.ReadLine();
-            Console.WriteLine("Welcome " + userName + "!!!");
-        }
+        
     }
 }
+
